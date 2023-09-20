58dd1c1dfba83759af56edcc3c8fe87de7ce2c63
Hatsune Qixeo <112535792+HatsuneQixeo@users.noreply.github.com>
2023-09-20 21:13:10 +0800

    1-00 assert?
    Make sense to assert because this should not be runtime error

--- 1-array/ex00/give_bmi.py
+++ 1-array/ex00/give_bmi.py
@@ -8,14 +8,13 @@ This function expects height in meters(m) and weight in kilograms(kg)."""
 
     np_height = NumPy.array(height)
     np_weight = NumPy.array(weight)
-    if np_height.size != np_weight.size:
-        raise IndexError("height and weight must have the same length")
-    elif np_height.dtype not in (NumPy.dtype('int'), NumPy.dtype('float')):
-        raise TypeError("height must be a list of numbers")
-    elif np_weight.dtype not in (NumPy.dtype('int'), NumPy.dtype('float')):
-        raise TypeError("weight must be a list of numbers")
-    else:
-        return (np_weight / (np_height ** 2)).tolist()
+    assert np_height.size == np_weight.size, \
+        "height and weight must have the same length"
+    assert np_height.dtype in (NumPy.dtype('int'), NumPy.dtype('float')), \
+        "height must be a list of numbers"
+    assert np_weight.dtype in (NumPy.dtype('int'), NumPy.dtype('float')), \
+        "weight must be a list of numbers"
+    return (np_weight / (np_height ** 2)).tolist()
 
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
@@ -23,10 +22,9 @@ def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
 representing whether the BMI is beyond the given limit."""
 
     arr = NumPy.array(bmi)
-    if arr.dtype not in (NumPy.dtype('int'), NumPy.dtype('float')):
-        raise TypeError("bmi must be a list of numbers")
-    else:
-        return (arr > limit).tolist()
+    assert arr.dtype in (NumPy.dtype('int'), NumPy.dtype('float')), \
+        "bmi must be a list of numbers"
+    return (arr > limit).tolist()
 
 
 # def give_bmi(height: list[int | float], weight: list[int | float]) \
@@ -34,21 +32,19 @@ representing whether the BMI is beyond the given limit."""
 #     """Returns a list of BMI values given a list of height and weight.
 # This function expects height in meters(m) and weight in kilograms(kg)."""
 
-#     if len(height) != len(weight):
-#         raise IndexError("height and weight must have the same length")
-#     elif not all(isinstance(ele, (int, float)) for ele in height):
-#         raise TypeError("height must be a list of numbers")
-#     elif not all(isinstance(ele, (int, float)) for ele in weight):
-#         raise TypeError("weight must be a list of numbers")
-#     else:
-#         return [w / (h ** 2) for h, w in zip(height, weight)]
+#     assert len(height) == len(weight), \
+#         "height and weight must have the same length"
+#     assert all(isinstance(ele, (int, float)) for ele in height), \
+#         "height must be a list of numbers"
+#     assert all(isinstance(ele, (int, float)) for ele in weight), \
+#         "weight must be a list of numbers"
+#     return [w / (h ** 2) for h, w in zip(height, weight)]
 
 
 # def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
 #     """Return a list of boolean value \
 # representing whether the BMI is beyond the given limit."""
 
-#     if not all(isinstance(ele, (int | float)) for ele in bmi):
-#         raise TypeError("bmi must be a list of numbers")
-#     else:
-#         return [ele > limit for ele in bmi]
+#     assert all(isinstance(ele, (int | float)) for ele in bmi), \
+#         "bmi must be a list of numbers"
+#     return [ele > limit for ele in bmi]
