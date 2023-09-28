5b74751f3826ba0a757513ae9bb33c1da3283062
Hatsune Qixeo <112535792+HatsuneQixeo@users.noreply.github.com>
2023-09-29 01:08:09 +0800

    clean up, docstring, bug fix

--- 3-oop/ex02/tester.py
+++ 3-oop/ex02/tester.py
@@ -1,5 +1,3 @@
-from S1E7 import Baratheon, Lannister
-from S1E9 import Character, Stark
 from DiamondTrap import King
 
 Joffrey = King("Joffrey")
@@ -9,11 +7,3 @@ Joffrey.set_hairs("light")
 print(Joffrey.get_eyes())
 print(Joffrey.get_hairs())
 print(Joffrey.__dict__)
-if (isinstance(Joffrey, King)
-    and issubclass(King, Character)
-    and isinstance(Joffrey, Baratheon)
-    and isinstance(Joffrey, Lannister)):
-    print("OK")
-else:
-    print("Something seems fishy, look at the code to see if \
-the class king is inherited from the previous exercises")
