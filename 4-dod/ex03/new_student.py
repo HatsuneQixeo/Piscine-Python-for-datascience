1cc8b629e5f76ff216d2ff95d2b934fb94a4ffb4
Hatsune Qixeo <112535792+HatsuneQixeo@users.noreply.github.com>
2023-09-29 01:59:38 +0800

    4-03

---
+++ 4-dod/ex03/new_student.py
@@ -0,0 +1,24 @@
+import random
+import string
+from dataclasses import dataclass, field
+
+
+def generate_id() -> str:
+    """Generates random id"""
+
+    return "".join(random.choices(string.ascii_lowercase, k = 15))
+
+
+@dataclass
+class Student:
+    """A student"""
+
+    name: str
+    surname: str
+    active: bool = True
+    login: str = field(init=False)
+    id: str = field(init=False, default_factory=generate_id)
+
+    def __post_init__(self):
+        """Generates login from name and surname"""
+        self.login = (self.name[0] if self.name else '') + self.surname
---
+++ 4-dod/ex03/tester.py
@@ -0,0 +1,14 @@
+from new_student import Student
+
+student = Student(name = "Edward", surname = "agle")
+print(student)
+
+# try:
+#     student = Student(name = "Edward", surname = "agle", login="Eagle")
+# except Exception as e:
+#     print(f"{e.__class__.__name__}:", e)
+
+# try:
+#     Student(name = "Edward")
+# except Exception as e:
+#     print(f"{e.__class__.__name__}:", e)
