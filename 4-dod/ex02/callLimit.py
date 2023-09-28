d8c55ed50e3db48927c424db1e237adfd68e16b8
Hatsune Qixeo <112535792+HatsuneQixeo@users.noreply.github.com>
2023-09-29 06:08:05 +0800

    4-02

---
+++ 4-dod/ex02/callLimit.py
@@ -0,0 +1,41 @@
+def callLimit(limit: int):
+    """Decorator for setting limits on function calls"""
+    count = 0
+
+    def callLimiter(function):
+        """The decorator call"""
+
+        def limit_function(*args, **kwargs):
+            """Limit the amount of time given function is called"""
+            nonlocal function
+            nonlocal count
+            nonlocal limit
+
+            if count >= limit:
+                print(f"Error: function {function.__name__} at {function}",
+                      "called too many times")
+                return
+            count += 1
+            return function(*args, **kwargs)
+        return limit_function
+    return callLimiter
+
+# class callLimit:
+#     """Decorator class for setting limits on function calls"""
+
+#     def __init__(self, limit: int):
+#         """callLimit constructor"""
+#         self.limit = limit
+#         self.count = 0
+
+#     def __call__(self, function):
+#         """Parantheses Overload"""
+#         def limit_function(*args, **kwargs):
+#             """Limit the amount of time given function is called"""
+#             if self.count >= self.limit:
+#                 print(f"Error: function {function.__name__} at {function}",
+#                       "called too many times")
+#                 return
+#             self.count += 1
+#             return function(*args, **kwargs)
+#         return limit_function
