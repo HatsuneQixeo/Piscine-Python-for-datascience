1f898df473df4399d4e4ecb39a50d518d5890a8d
Hatsune Qixeo <112535792+HatsuneQixeo@users.noreply.github.com>
2023-09-29 06:16:44 +0800

    4-03 more test case

--- 4-dod/ex03/tester.py
+++ 4-dod/ex03/tester.py
@@ -3,8 +3,15 @@ from new_student import Student
 student = Student(name="Edward", surname="agle")
 print(student)
 
+# student = Student(name = "Edward", surname = "agle", active=False)
+
 # try:
 #     student = Student(name = "Edward", surname = "agle", login="Eagle")
+# except Exception as e:
+#     print(f"{e.__class__.__name__}:", e)
+
+# try:
+#     student = Student(name = "Edward", surname = "agle", id="myid")
 # except Exception as e:
 #     print(f"{e.__class__.__name__}:", e)
 
