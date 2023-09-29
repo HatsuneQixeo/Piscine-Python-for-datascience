c3c0dd13cebd5a44dd8fadb9f0fca0c4434428d6
Hatsune Qixeo <112535792+HatsuneQixeo@users.noreply.github.com>
2023-09-30 00:34:25 +0800

    4 flake8

--- 4-dod/ex01/in_out.py
+++ 4-dod/ex01/in_out.py
@@ -1,27 +1,31 @@
 def square(x: int | float) -> int | float:
-	"""Return the square of a number"""
-	return x ** 2
+    """Return the square of a number"""
+
+    return x ** 2
 
 
 def pow(x: int | float) -> int | float:
-	"""Return the result of pow(x, x)"""
-	return x ** x
+    """Return the result of pow(x, x)"""
+
+    return x ** x
 
 
 def outer(x: int | float, function) -> object:
-	"""Return a closure that
-		- keep track of the amount of times it was called,
-		- redefine x with the result of function(x),
-		- and return the result of function(x) every call
-	"""
+    """Return a closure that
+        - keep track of the amount of times it was called,
+        - redefine x with the result of function(x),
+        - and return the result of function(x) every call
+    """
 
-	count = 0
-	def inner() -> float:
-		"""The closure"""
-		nonlocal count
-		nonlocal x
+    count = 0
 
-		count += 1
-		x = function(x)
-		return x
-	return inner
+    def inner() -> float:
+        """The closure"""
+
+        nonlocal count
+        nonlocal x
+
+        count += 1
+        x = function(x)
+        return x
+    return inner
