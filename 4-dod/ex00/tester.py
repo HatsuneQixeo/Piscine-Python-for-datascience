bad6844479214e2260390e663f805c4e4538ce52
HatsuneQixeo <112535792+HatsuneQixeo@users.noreply.github.com>
2023-09-29 21:46:49 +0800

    4-(00-01)

---
+++ 4-dod/ex00/tester.py
@@ -0,0 +1,25 @@
+from statistics import ft_statistics
+
+ft_statistics(1, 42, 360, 11, 64, toto="mean", tutu="median", tata="quartile")
+print("-----")
+ft_statistics(5, 75, 450, 18, 597, 27474, 48575, hello="std", world="var")
+print("-----")
+ft_statistics(5, 75, 450, 18, 597, 27474, 48575, ejfhhe="heheh", ejdjdejn="kdekem")
+print("-----")
+ft_statistics(toto="mean", tutu="median", tata="quartile")
+
+# ft_statistics(42, 115, 30, 151, 6400, toto='mean', tutu='std', tata='quartile', titi='djdj')
+# print("-----")
+# ft_statistics(51, 735, 455, 8, 7, 74, 75, hello="median", world="var")
+# print("-----")
+# ft_statistics(5, 75, 450, 18, 597, 27474, 48575, ejfhhe="heheh", ejdjdejn="kdekem")
+# print("-----")
+# ft_statistics(hello='std', world='median')
+
+# import statistics
+# print(statistics.mean([1, 42, 360, 11, 64]))
+# print(statistics.median([1, 42, 360, 11, 64]))
+# print(statistics.quantiles([1, 42, 360, 11, 64]))
+
+# print(statistics.stdev([5, 75, 450, 18, 597, 27474, 48575]))
+# print(statistics.variance([5, 75, 450, 18, 597, 27474, 48575]))
